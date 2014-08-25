97fa470e7e5781f7e5c588692a139034cf5a61aa
root <root@hesteserver.sofienhaug.no>
2014-08-25 18:14:03 +0200

    Fixed config

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -d -v $1:/var/lib/mysql --name db tutum/mysql:5.6
+docker run -d -v $1:/var/lib/mysql -e MYSQL_PASS="hester er fine" --name db tutum/mysql:5.6
 
-docker run -d -p 8080:8080 -p 4848:4848 -p 8181:8181 --name web --link db:db knutesten/sp_javaee:v1.0
+docker run -d -p 80:8080 -p 4848:4848 -p 81:8181 --name web --link db:db knutesten/sp_javaee:v1.0
