611075f6ea985a173fea04cbc46252235a455ffa
Knut Esten Melandsø Nekså <knutn@mesan.no>
2014-12-04 18:48:26 +0100

    Changed startup-script with fig.yml

--- bin/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker run -d -v $1:/var/lib/mysql -e MYSQL_PASS="hester er fine" --name db tutum/mysql:5.6
-
-docker run -d -p 80:8080 -p 4848:4848 -p 81:8181 --name web --link db:db knutesten/sp_javaee:v1.0
